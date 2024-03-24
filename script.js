9179e8897d8143768d4d68d884f7832f5f740fde
miguelruizc <buumchakalaka@gmail.com>
2024-03-24 18:48:32 +0100

    Add gridSizeButton that prompts for a size and check if is a valid input

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-const GRID_SIZE = 16; // 16*16 square grid
+const DEFAULT_GRID_SIZE = 16; // 16*16 square grid
+
+// Grid size button
+let gridSize = DEFAULT_GRID_SIZE;
+let gridSizeButton = document.querySelector(".gridSizeButton");
+
+gridSizeButton.addEventListener("click", () => {
+    let userInput = window.prompt("Enter number of squares per side: ");
+    
+    userInput = parseInt(userInput);
+
+    if(isNaN(userInput) || userInput < 0){
+        gridSize = DEFAULT_GRID_SIZE;
+        console.log(gridSize);
+        return;
+    }
+    else if(userInput > 100) {
+        gridSize = 100;
+        console.log(gridSize);
+        return;
+    }
+    else {
+        gridSize = userInput;
+        console.log(gridSize);
+        return;
+    }
+});
 
 // Container holding the grid 
 let containerDiv = document.querySelector(".container");
 
-// Iterate all grid size
-for(let i = 0; i < GRID_SIZE; i++)
+// Iterate grid
+for(let i = 0; i < DEFAULT_GRID_SIZE; i++)
 {
-    for(let j = 0; j < GRID_SIZE; j++)
+    for(let j = 0; j < DEFAULT_GRID_SIZE; j++)
     {
         // Create new gridDiv
         let newDiv = document.createElement("div");
