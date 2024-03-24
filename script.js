af56896ce3dcf3876a9e26b52adc5eea677235d3
miguelruizc <buumchakalaka@gmail.com>
2024-03-24 18:10:09 +0100

    Add 16*16 grid of divs created with js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Javascript code!");
+const GRID_SIZE = 16; // 16*16 square grid
+
+let containerDiv = document.querySelector(".container");
+
+for(let i = 0; i < GRID_SIZE; i++)
+{
+    for(let j = 0; j < GRID_SIZE; j++)
+    {
+        let newDiv = document.createElement("div");
+    
+        newDiv.classList.add("gridDiv");
+
+        containerDiv.appendChild(newDiv);
+    }
+}
